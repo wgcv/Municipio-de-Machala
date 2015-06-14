49b235a0e4453532b93dbd09fbd3a72f1341d6d3
Gustavo Cevallos <wgcv@wgcv.me>
2015-06-13 21:10:33 -0500

    Se creo el menu

---
+++ app/src/main/java/me/wgcv/municipiomachala/Menu.java
@@ -0,0 +1,7 @@
+package me.wgcv.municipiomachala;
+
+/**
+ * Created by wgcv on 13/06/15.
+ */
+public class Menu {
+}
---
+++ app/src/main/java/me/wgcv/municipiomachala/Principal.java
@@ -0,0 +1,76 @@
+package me.wgcv.municipiomachala;
+
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class Principal extends ActionBarActivity {
+
+    public Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private ListView leftDrawerList;
+    private ArrayAdapter<String> navigationDrawerAdapter;
+    private String[] menu = {"Home", "Android", "Sitemap", "About", "Contact Me"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        nitView();
+        if (toolbar != null) {
+            toolbar.setTitle("Municipio de Machala");
+            setSupportActionBar(toolbar);
+        }
+        initDrawer();
+    }
+
+    private void nitView() {
+        leftDrawerList = (ListView) findViewById(R.id.left_drawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationDrawerAdapter=new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, menu);
+        leftDrawerList.setAdapter(navigationDrawerAdapter);
+    }
+
+    private void initDrawer() {
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+}
